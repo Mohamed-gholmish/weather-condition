9a03bb2b33a11eea55fed378521502696af5f710
Ziad Gholmish <ZiadGholmish@gmail.com>
2023-03-04 03:22:12 +0200

    search

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
-
 let res;
 let finalRes;
 
-async function getWeather(city){
-res=await fetch(`https://api.weatherapi.com/v1/current.json?key=ed134372d9ca436abb0173133232602&q=${city}&days=1`);
- finalRes=await res.json()
- const {temp_f}=finalRes.current;
- const {text}=finalRes.current.condition;
+let search=document.querySelector("#search");
+
+search.addEventListener("search",(e)=>{
+  let cit=search.value;
+  console.log(cit)
+  getWeather(cit);
+})
 
 
-console.log(temp_f,text)
+async function getWeather(city) {
+  console.log(city)
+  if(city===''||city==undefined){
+    city='london'
+  }
+  res = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=ed134372d9ca436abb0173133232602&q=${city}&days=1`
+  );
+  finalRes = await res.json();
+  const { temp_f } = finalRes.current;
+  const { text } = finalRes.current.condition;
+  const { icon } = finalRes.current.condition;
+  document.querySelector("#cit").innerHTML=city;
+  let img = document.querySelector("#img");
+  img.setAttribute("src", `${icon}`);
 
+  console.log(temp_f, text);
 }
 
-getWeather("london");
-
- 
+getWeather();
